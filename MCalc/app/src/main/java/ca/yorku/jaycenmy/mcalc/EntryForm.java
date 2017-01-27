554d7494ef381963b57fcfaeb4f5d628075ddaa9
jaycenca <jaycenca@gmail.com>
2017-01-27 14:19:44 -0500

    finalized

--- MCalc/app/src/main/java/ca/yorku/jaycenmy/mcalc/EntryForm.java
+++ MCalc/app/src/main/java/ca/yorku/jaycenmy/mcalc/EntryForm.java
@@ -1,104 +1,69 @@
-package ca.yorku.jaycenmy.mcalc;
+package ca.roumani.mcalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import hr.YumModel;
 
-/**
- * Created by user on 1/23/17.
- */
-public class EntryForm
+public class EntryForm extends AppCompatActivity
 {
-    /**
-     * 3 private attributes to hold the principle (a double), the amortization (an int) and
-     * annual interest(double)\
-     *
-     * where p is the principle, r is the monthly interest
-     * and n is the number of months in the amortization
-     */
-    private static double P , R;
-    private static int n;
 
     /**
-     * Description: parameterized constructor accept three parameter n, p r, all strings
-     * and convert and stored them accordingly
-     * Method: Using the parseDouble and parseInt in Double and Integer classes to convert
-     *        them to the correct states ACCORDINGLY.
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    public EntryForm(String a , String b, String c)
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
     {
-        P = Double.parseDouble(a);
-        R = (Double.parseDouble(c) / 12) / 100;
-        n = Integer.parseInt(b) * 12;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_form);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    /**
-     * Description: ComputePament() method that returns the monthly payments as a string
-     * Formula:
-     *      (rP)
-     *   ___________________________
-     *     1 -  _______1__________
-     *          1 + nr + n(n-1)r^2/2
-     *
-     * Method:
-     *  1)change the r, the monthly interest rate to right decimal places
-     *  2)declare a few variables (double) to represent nominator and denominator, so that
-     *   it is more organize
-     */
-
-    public String computePayment()
+    public void buttonClicked(View v)
     {
-        double result = 1;
-        double nom = R * P;
-        double denom1 = (1)/((1 + (n * R) + (( n * (n - 1) * (Math.pow(R,2))/2))));
-        double denom2 = 1 - denom1;
-        //System.out.println("The Rate value , R is "+R );
-        //System.out.println("The Principle value, P is "+P);
-        //System.out.println("The Interest  value , n is "+n);
 
-        result =  (nom) / (denom2);
-        //System.out.println("The Result is "+result);
-        return String.format("%.2f", result);
+        String p = ((EditText) findViewById(R.id.principleBox)).getText().toString();
+        String n = ((EditText) findViewById(R.id.amortBox)).getText().toString();
+        String r = ((EditText) findViewById(R.id.interestBox)).getText().toString();
+
+
+        MortgageModel model = new MortgageModel(p, n, r);
+        String answer = "$"+model.getPayment();
+
+        ((TextView) findViewById(R.id.answer)).setText(answer);
+
     }
 
-    /**
-     *                  Optional Part of the lab
-     *  Description: an method called computeExactPayment() is created to compute the monthly payment
-     *              using the exact formula:
-     *                            rP
-     *           __________________________________
-     *                        1- (1 + r)^-n
-     */
-
-    public String computeExactPayment()
+    public void YumbuttonCicked(View v)
     {
-        double result = 1;
-        double nom = R * P;
-        double denom = 1 - (Math.pow( 1 + R , -n ));
+        System.out.println("start");
+        String p = ((EditText) findViewById(R.id.principleBox)).getText().toString();
+        String n = ((EditText) findViewById(R.id.amortBox)).getText().toString();
+        String r = ((EditText) findViewById(R.id.interestBox)).getText().toString();
 
-        result = (nom) / (denom);
+        YumModel model = new YumModel();
 
-        return String.format("%.2f", result);
+        model.setPrinciple(p);
+        model.setAmortization(n);
+        model.setInterest(r);
+
+        /*System.out.println("principle is "+model.);
+        System.out.println("amort is "+n);
+        System.out.println("interest is "+r);*/
+
+        String answer = model.computePayment();
+        ((TextView) findViewById(R.id.yumans)).setText(answer);
     }
 
-    /**
-     * Testing in the main
-     */
-    public static void main(String[]args)
-    {
-        //instantiate a new class using the parameterized constructor
-        EntryForm test1 = new EntryForm("700000","25","2.75");
-
-        //testing the computePayment() method
-        System.out.println("The Approximate value of EntryForm(\"700000\",\"25\",\"2.75\") is "+test1.computePayment());
-
-        //testing the computeExactPayment() method
-        System.out.println("The Exact value of EntryForm(\"700000\",\"25\",\"2.75\") is "+test1.computeExactPayment());
-
-        //testing the YUM class in the YUM jar
-        YumModel test2 = new YumModel();
-        test2.setPrinciple("700000");
-        test2.setAmortization("25");
-        test2.setInterest("2.75");
-
-        System.out.println("The Approximate value for YUM is "+test2.computePayment());
-    }
 }
--- MCalc/app/src/main/java/ca/yorku/jaycenmy/mcalc/MortgageModel.java
+++ MCalc/app/src/main/java/ca/yorku/jaycenmy/mcalc/MortgageModel.java
@@ -1,16 +1,112 @@
-package ca.yorku.jaycenmy.mcalc;
+package ca.roumani.mcalc;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import java.io.PrintStream;
+import java.util.Scanner;
+import hr.YumModel;
 
-public class MortgageModel extends AppCompatActivity
-{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
+
+    public class MortgageModel
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mortage);
+        /**
+         * 3 private attributes to hold the principle (a double), the amortization (an int) and
+         * annual interest(double)\
+         *
+         * where p is the principle, r is the monthly interest
+         * and n is the number of months in the amortization
+         */
+        private double p;
+        private double r;
+        private int n;
+
+        /**
+         * Description: parameterized constructor accept three parameter n, p r, all strings
+         * and convert and stored them accordingly
+         * Method: Using the parseDouble and parseInt in Double and Integer classes to convert
+         *        them to the correct states ACCORDINGLY.
+         */
+        public MortgageModel (String a , String b, String c)
+        {
+            p = Double.parseDouble(a);
+            n = Integer.parseInt(b) * 12;
+            r = (Double.parseDouble(c) / 12) / 100;
+
+        }
+
+        /**
+         * Description: ComputePament() method that returns the monthly payments as a string
+         * Formula:
+         *      (rP)
+         *   ___________________________
+         *     1 -  _______1__________
+         *          1 + nr + n(n-1)r^2/2
+         *
+         * Method:
+         *  1)change the r, the monthly interest rate to right decimal places
+         *  2)declare a few variables (double) to represent nominator and denominator, so that
+         *   it is more organize
+         */
+
+        public String getPayment()
+        {
+            double result = 1;
+            double nom = r * p;
+            double denom1 = (1)/((1 + (n * r) + (( n * (n - 1) * (Math.pow(r,2))/2))));
+            double denom2 = 1 - denom1;
+            //System.out.println("The Rate value , R is "+R );
+            //System.out.println("The Principle value, P is "+P);
+            //System.out.println("The Interest  value , n is "+n);
+
+            result =  (nom) / (denom2);
+            //System.out.println("The Result is "+result);
+            return String.format("%,.2f", result);
+
+        }
+
+        /**
+         *                  Optional Part of the lab
+         *  Description: an method called computeExactPayment() is created to compute the monthly payment
+         *              using the exact formula:
+         *                            rP
+         *           __________________________________
+         *                        1- (1 + r)^-n
+         */
+
+        public String computeExactPayment()
+        {
+            double result = 1;
+            double nom = r * p;
+            double denom = 1 - (Math.pow( 1 + r , -n ));
+
+            result = (nom) / (denom);
+
+            return String.format("%,.2f", result);
+        }
+
+        /**
+         * Testing in the main
+         */
+
+        public static void main(String[] args)
+        {
+            Scanner input = new Scanner(System.in);
+            PrintStream output = System.out;
+
+            MortgageModel model = new MortgageModel("700000", "25", "2.75");
+            output.println(model.getPayment());
+
+            //testing the YUM class in the YUM jar
+            YumModel test2 = new YumModel();
+            test2.setPrinciple("700000");
+            test2.setAmortization("25");
+            test2.setInterest("2.75");
+            System.out.println("The Approximate value for YUM is "+test2.computePayment());
+
+        }
+
+        //optional
+        //will discussed later
+
     }
 
-}
+
